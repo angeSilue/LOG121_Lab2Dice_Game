8f90c1962faa6885eafda0e4c70a4f1e48067426
angeSilue <49754522+angeSilue@users.noreply.github.com>
2020-02-26 14:39:58 -0500

    Update CollectionJoueurs.java

--- src/framework/CollectionJoueurs.java
+++ src/framework/CollectionJoueurs.java
@@ -1,25 +1,31 @@
 package framework;
 
-import java.util.Iterator;
+import java.lang.reflect.Array;
 
 public class CollectionJoueurs {
 
+    static int taille = 1 ;
+   static Joueur[] joueursTab = new Joueur[taille];
 
-    public CollectionJoueurs() {
+    public static void ajouterJoueurs(Joueur joueur){
+
+
+            int i = joueursTab.length;
+            joueursTab[i-1] = joueur;
+            for(Joueur j : joueursTab){
+                System.out.println(j.nom);
+            }
+        taille++;
+
 
     }
-
-    public void ajouterJoueur(Joueur joueur) {
-
-
+    public static void main(String[] args) {
+        Joueur j = new Joueur("a",2,2);
+        Joueur j1 = new Joueur("b",2,2);
+        ajouterJoueurs(j);
+        ajouterJoueurs(j1);
+        ajouterJoueurs(j1);
+        ajouterJoueurs(j1);
     }
-
-    public Iterator creerIterateur() {
-
-        return null;
-    }
-
-
-
 
 }
