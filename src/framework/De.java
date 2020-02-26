852d8608508df777a45693db76fba80f1125220b
angeSilue <49754522+angeSilue@users.noreply.github.com>
2020-02-26 14:39:26 -0500

    Update De.java

--- src/framework/De.java
+++ src/framework/De.java
@@ -1,20 +1,22 @@
 package framework;
 
-public class De implements Comparable {
+public class De implements Comparable<De> {
+    int nbFaces;
+    int idDes;
+    int faceActu;
 
-
-    private int nbFaces;
-
-    public De() {
-
+    De (int nbFaces, int id){
+        this.nbFaces = nbFaces;
+        this.idDes = id;
     }
 
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(De de) {
+        if(faceActu == de.faceActu)
+            return 0;
+        else if(faceActu >  de.faceActu)
+            return 1;
+        else
+            return -1;
     }
-
-
-
-
 }
